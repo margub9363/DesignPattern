25daad0ccd89ddf929ba8f9c86c33449138009da
margub9363 <69838623+margub9363@users.noreply.github.com>
2023-05-11 10:25:27 +0530

    Builder Design Pattern

---
+++ untitled/src/main/java/BuilderDesignPattern/com/Phone/Shop.java
@@ -0,0 +1,8 @@
+package BuilderDesignPattern.com.Phone;
+
+public class Shop {
+    public static void main(String[] args) {
+        Phone p = new PhoneBuilder().setProcessor("Abc").setOs("Android").setRam(2).getPhone();
+        System.out.println(p);
+    }
+}
